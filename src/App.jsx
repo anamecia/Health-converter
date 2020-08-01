0fbb98f1dca6df4d573e354d77c5b62064c2e57c
anamecia <anameciarobalo@gmail.com>
2020-08-01 22:23:51 +0100

    Add header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./sass/main.scss"
+
+import Header from "./components/header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <div className="header">
+      <div>
+        <div className="header__logo">
+          <span>Health </span>
+          <span>converter</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
